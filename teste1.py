e06f4448868c36b8a306fdc2ffee8b57dd19c088
letsdelfino <41598982+letsdelfino@users.noreply.github.com>
2019-07-31 17:31:00 -0300

    tipos de variáveis
    aula 06 do curso da Udemy

--- teste1.py
+++ teste1.py
@@ -12,3 +12,23 @@ print(5/1)
 # expoente
 print(2 ** 3)
 print(3 ** 3)
+
+# resto da operação
+print(10 % 3)
+
+string = "Olá mundo"
+print(string)
+print(string)
+print(string)
+print(string)
+print(string)
+
+var1 = 1 # inteiro
+var2 = 1.1 # float
+
+print (var1)
+print(var2)
+
+var3 = True # bool
+
+print(var3)
