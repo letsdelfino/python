50451c988b64b91bb4c3714ff4882d7de6056f41
letsdelfino <41598982+letsdelfino@users.noreply.github.com>
2019-08-06 10:32:09 -0300

    manipulacao de arquivo
    aula 21 da Udemy

--- arquivo4.py
+++ arquivo4.py
@@ -5,3 +5,15 @@ def soma(x, y):
     print(x+y)
 
 soma(3,4)
+
+# manipulação de arquivos
+
+arquivo = open("Arquivo.txt", "a+")
+
+arquivo.write("Esse é o meu arquivo")
+linhas = arquivo.readlines()
+print(linhas)
+texto = arquivo.read()
+print(texto)
+
+arquivo.close()
